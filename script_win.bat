082f4b47736ad48d907ca22a8e650b0470cb8ce7
Serms1999 <serms1999@gmail.com>
2023-07-30 21:57:17 +0200

    Windows mode finished

--- script_win.bat
+++ script_win.bat
@@ -1,8 +1,23 @@
-set outputFolder="Ficheros Salida"
+@echo off
 
-if exist %outputFolder% (
-    rmdir %outputFold% /s /q
+set executableFile=Practica2.exe
+set outputFolder="Ficheros Salida"
+set situations=I F
+
+if exist %executableFile% (
+    if exist %outputFolder% (
+        rmdir %outputFolder% /s /q
+    )
+    mkdir %outputFolder%
+
+    for %%s in (%situations%) do (
+        for /l %%n in (1, 1, 4) do (
+             call %executableFile% Situaciones\BC-%%s.txt Situaciones\BH-%%s%%n.txt Situaciones\Config-%%s.txt
+             move Salida*.txt %outputFolder%
+        )
+    )
+) else (
+    echo Ejecutable %executableFile% no encontrado
 )
-mkdir %outputFolder%
 
 pause
